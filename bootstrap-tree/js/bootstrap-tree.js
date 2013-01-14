089ee43cfdd5e071a7e432504d5d3ff242597cf4
Ilia Choly <icholy@accipiterradar.com>
2013-01-14 15:31:43 -0500

    remove trailing whitespace

--- bootstrap-tree/js/bootstrap-tree.js
+++ bootstrap-tree/js/bootstrap-tree.js
@@ -1,7 +1,7 @@
 /* =============================================================
  * bootstrap-tree.js v0.3
  * http://twitter.github.com/cutterbl/Bootstrap-Tree
- * 
+ *
  * Inspired by Twitter Bootstrap, with credit to bits of code
  * from all over.
  * =============================================================
@@ -28,7 +28,7 @@
    * ========================= */
 
   var Tree = function (element, options) {
-    
+
     this.$element = $(element);
     this.$tree = this.$element.closest(".tree");
     this.parentage = GetParentage(this.$element);
@@ -45,14 +45,14 @@
 
     constructor: Tree
 
-    , toggle: function () {      
+    , toggle: function () {
       var a, n, s
         , currentStatus = this.$element.hasClass("in")
         , eventName = (!currentStatus) ? "openbranch" : "closebranch";
-          
+
       this.$parent[currentStatus ? "addClass" : "removeClass"]("closed");
       this.$element[currentStatus ? "removeClass" : "addClass"]("in");
-      
+
       n = this.node();
       // 'Action' (open|close) event
       a = $.Event(eventName, {
@@ -62,140 +62,140 @@
       s = $.Event("nodeselect", {
         node: n
       });
-      
-      this.$parent.trigger(a).trigger(s);      
-    }    
-    , _buildOutput: function (doc, type, parent) {      
+
+      this.$parent.trigger(a).trigger(s);
+    }
+    , _buildOutput: function (doc, type, parent) {
       var nodes = this._buildNodes(doc, type);
       parent.empty().append(this._createNodes(nodes));
-    }    
+    }
     , _createNodes: function (nodes) {
-      
+
       var els = []
         , $this = $(this);
-      
-      $.each(nodes, function (ind, el) {        
+
+      $.each(nodes, function (ind, el) {
         var node = $("<li>")
           , role = (el.leaf) ? "leaf" : "branch"
           , attributes = {}
           , anchor = $("<a>");
-        
+
         attributes.role = role;
-        
-        if (!el.leaf) {          
-          var branch = $("<ul>").addClass("branch");          
+
+        if (!el.leaf) {
+          var branch = $("<ul>").addClass("branch");
           attributes.class = "tree-toggle closed";
-          attributes["data-toggle"] = "branch";            
+          attributes["data-toggle"] = "branch";
         }
-        
-        if (el.value) attributes["data-value"] = el.value;          
+
+        if (el.value) attributes["data-value"] = el.value;
         if (el.id) attributes["data-itemid"] = el.id;
-        
-        for (var key in el) { // do we have some extras?          
-          if (key.indexOf("data-") !== -1) attributes[key] = el[key];          
-        }        
-          
+
+        for (var key in el) { // do we have some extras?
+          if (key.indexOf("data-") !== -1) attributes[key] = el[key];
+        }
+
         // trade the anchor for a span tag, if it's a leaf
         // and there's no href
-        if (el.leaf) {          
+        if (el.leaf) {
           anchor = $("<span>");
         } else {
           attributes.href = "#";
         }
-        
+
         anchor.attr(attributes);
-        
+
         if (el.cls) anchor.addClass(el.cls);
-        if (!el.leaf && el.expanded && el.children.length) {          
+        if (!el.leaf && el.expanded && el.children.length) {
           anchor.removeClass("closed");
-          branch.addClass("in");          
-        }
-        
-        anchor.html(el.text);
-        node.append(anchor);
-        
-        if (!el.leaf && el.children && el.children.length) {          
-          branch.append($this[0]._createNodes(el.children));
-          node.append(branch);          
+          branch.addClass("in");
         }
 
-        els.push(node);        
+        anchor.html(el.text);
+        node.append(anchor);
+
+        if (!el.leaf && el.children && el.children.length) {
+          branch.append($this[0]._createNodes(el.children));
+          node.append(branch);
+        }
+
+        els.push(node);
       });
-      
+
       return els;
-    }    
-    , _buildNodes: function (doc, type) {      
+    }
+    , _buildNodes: function (doc, type) {
       var nodes = []
         , $el = this.$element;
-      
-      if (type === "json") {        
+
+      if (type === "json") {
         nodes = this._parseJsonNodes(doc);
       } else if (type === "xml") {
         nodes = this._parseXmlNodes($(doc).find("nodes").children());
       }
       return nodes;
-    }    
-    , _parseJsonNodes: function (doc) {      
+    }
+    , _parseJsonNodes: function (doc) {
       var nodes = []
         , $this = $(this);
-      
-      $.each(doc, function (ind, el) {        
+
+      $.each(doc, function (ind, el) {
         var opts = {}
           , boolChkArr = ["leaf","expanded","checkable","checked"];
-        
-        for (var item in el) {          
-          var nodeVal = (item !== "children") ? el[item] : $this[0]._parseJsonNodes(el.children);              
+
+        for (var item in el) {
+          var nodeVal = (item !== "children") ? el[item] : $this[0]._parseJsonNodes(el.children);
           if (!$.isArray(nodeVal)) nodeVal = $.trim(nodeVal);
-          if (nodeVal.length) opts[item] = ($.inArray(item, boolChkArr) > -1) ? SetBoolean(nodeVal) : nodeVal;              
+          if (nodeVal.length) opts[item] = ($.inArray(item, boolChkArr) > -1) ? SetBoolean(nodeVal) : nodeVal;
         }
-        
+
         nodes.push(new Node(opts));
       });
-      
-      return nodes;      
-    }    
-    , _parseXmlNodes: function (doc) {      
+
+      return nodes;
+    }
+    , _parseXmlNodes: function (doc) {
       var nodes = []
         , $this = $(this)
         , boolChkArr = ["leaf","expanded","checkable","checked"];
-      
+
       $.each(doc, function (ind, el) {
-        
+
         var opts = {}
           , $el = $(el);
-        
+
         $.each($el.children(), function (x, i) {
-          
+
           var $i = $(i)
             , tagName = $i[0].nodeName
             , nodeVal = (tagName !== "children") ? $i.text() : $this[0]._parseXmlNodes($i.children("node"));
-                
+
           if (!$.isArray(nodeVal)) nodeVal = $.trim(nodeVal);
-          if (nodeVal.length) opts[tagName] = ($.inArray(tagName, boolChkArr) > -1) ? SetBoolean(nodeVal) : nodeVal;              
-        });        
+          if (nodeVal.length) opts[tagName] = ($.inArray(tagName, boolChkArr) > -1) ? SetBoolean(nodeVal) : nodeVal;
+        });
         nodes.push(new Node(opts));
-      });      
-      return nodes;      
+      });
+      return nodes;
     }
-    , getparentage: function () {      
-      return this.parentage;      
+    , getparentage: function () {
+      return this.parentage;
     }
     , node: function (el) {
       el = el || $(this);
-      
+
       var node = $.extend(true, {}, (el[0] === $(this)[0]) ? $(this.$parent).data() : el.data());
-      
+
       node.branch = this.$element;
       node.parentage = this.parentage;
       node.el = (el[0] === $(this)[0]) ? this.$parent : el;
-      
+
       delete node.parent;
-      
+
       return node;
     }
   }
-  
-  var Node = function (options) {    
+
+  var Node = function (options) {
     $.extend(true, this, {
       text: undefined,
       leaf: false,
@@ -207,40 +207,40 @@
       checkable: false,
       checked: false,
       children: []
-    }, options);    
+    }, options);
   }
 
-  var GetParentBranch = function ($this) {    
+  var GetParentBranch = function ($this) {
     return $this.closest("ul.branch").prev(".tree-toggle");
   }
 
   var GetParentage = function ($this) {
-    
+
     var arr = [], tmp;
-    
+
     tmp = GetParentBranch($this);
     if (tmp.length) {
       arr = GetParentage(tmp);
       arr.push(tmp.attr("data-value")||tmp.text());
     }
-    
-    return arr;    
+
+    return arr;
   }
-  
+
   /**
    * FUNCTION SetBoolean
-   * 
+   *
    * Takes any value, and returns it's boolean equivalent.
-   * 
+   *
    * @param value (any)
    * @return (boolean)
    */
   var SetBoolean = function (value) {
-    
+
     value = $.trim(value);
-    
-    if (typeof value === "undefined" || value === null) return false;    
-    if (typeof value === "string" && !isNaN(value)) value = parseFloat(value);    
+
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && !isNaN(value)) value = parseFloat(value);
     if (typeof value === "string") {
       switch (value.toLowerCase()) {
         case "true":
@@ -251,7 +251,7 @@
           return false;
       }
     }
-    
+
     return Boolean(value);
   }
 
@@ -259,18 +259,18 @@
   /* COLLAPSIBLE PLUGIN DEFINITION
    * ============================== */
 
-  $.fn.tree = function (option) {    
+  $.fn.tree = function (option) {
     return this.each(function () {
       var $this = $(this)
         , data = $this.data("tree")
         , options = typeof option == "object" && option
-        
+
       if (!data) $this.data("tree", (data = new Tree(this, options)))
       if (typeof option == "string") data[option]()
-    });    
+    });
   };
 
-  $.fn.tree.defaults = {      
+  $.fn.tree.defaults = {
     toggle: true
   };
 
@@ -280,48 +280,47 @@
    * ==================== */
 
   $(function () {
-    
+
     $("body").on("click.tree.data-api", "[data-toggle=branch]", function (e) {
-      
+
       e.preventDefault();
-      
+
       var $this = $(this)
         , target = $this.next(".branch")
         , option = $(target).data("tree") ? "toggle" : $this.data();
-        
-      
+
       option.parent = $this;
       option.href = undefined;
-          
+
       $(target).tree(option);
-      
+
       return false;
     })
-    
+
     $("body").on("click.tree.data-api", "[role=leaf]", function (e) {
-      
+
       var $this = $(this)
         , branch = $this.closest(".branch");
-        
+
       // If not initialized, then create it
       if (!$(branch).data("tree")) {
-        
+
         var $target = $(branch)
           , branchlink = $target.prev("[data-toggle=branch]")
           , branchdata = branchlink.data();
-        
+
         $target.tree($.extend({}, branchdata, {
           "toggle": false,
           "parent": branchlink
         }));
       }
-      
+
       e = $.Event("nodeselect", {
         node: $(branch).data("tree").node($this)
       });
-      
-      $this.trigger(e);      
-    });    
+
+      $this.trigger(e);
+    });
   });
 
 }(window.jQuery);
